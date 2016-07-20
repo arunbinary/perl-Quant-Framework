4f85c36e424bcc15e239064dc2c960f413fc7e9e
Mahdi Mohammadi <mm@binary.com>
2016-07-20 01:33:20 +0000

    remove spot

--- lib/Quant/Framework/Utils/UnderlyingConfig.pm
+++ lib/Quant/Framework/Utils/UnderlyingConfig.pm
@@ -118,16 +118,6 @@ has uses_implied_rate => (
     isa => 'Maybe[Bool]',
 );
 
-=head2 spot
-
-Current spot price
-
-=cut
-
-has spot => (
-    is => 'ro',
-);
-
 =head2 asset_symbol
 
 Symbol name of the asset (Asset or Currency)
